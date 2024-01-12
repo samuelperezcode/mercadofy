74a9ced1cd4b463b8173243b70455440cc62a91a
SamyCode <sambea2103@gmail.com>
2024-01-12 16:52:38 -0500

    style(home): add styles to center content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,12 @@ import { Wrapper } from "@/components/wrapper";
 export default function Home() {
   return (
     <Wrapper>
-      <h1 className="text-3xl text-red-500">Mercadofy</h1>
-      <Button>
-        Add
-      </Button>
+      <div className="flex flex-col mx-auto py-20 text-center items-center max-w-3xl">
+        <h1 className="text-3xl text-red-500">Mercadofy</h1>
+        <Button>
+          Add
+        </Button>
+      </div>
     </Wrapper>
 
   )
